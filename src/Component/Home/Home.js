e82fd2b88d83fe1a101d3934d97b2f86db414e55
Mahmud Hasan Imran <mahmud.bubt.150@gmail.com>
2022-09-30 05:09:43 +0600

    Added Profile Detail

--- src/Component/Home/Home.js
+++ src/Component/Home/Home.js
@@ -1,26 +1,28 @@
 import React, { useEffect, useState } from 'react';
 import ExerciseData from '../ExerciseData/ExerciseData';
+import proPic from '../Assests/pro-pic.jpg';
+import { FiMapPin } from 'react-icons/fi';
 
 const Home = () => {
     const [exercise, setExercise] = useState([]);
     const [timeCount, setTimeCount] = useState([]);
-    useEffect(()=>{
+    useEffect(() => {
         fetch('data.json')
-        .then(res => res.json())
-        .then(data => setExercise(data))
-    },[]);
+            .then(res => res.json())
+            .then(data => setExercise(data))
+    }, []);
     // console.log(exercise)
     console.log(timeCount);
-    const add = (allData) =>{
+    const add = (allData) => {
         console.log(timeCount);
         const count = [...timeCount, allData.time];
         setTimeCount(count)
-   
+
     }
-         let sum = timeCount.reduce(function(a, b){
-            return a+b;
-        },0);
-        console.log(sum)
+    let sum = timeCount.reduce(function (a, b) {
+        return a + b;
+    }, 0);
+    console.log(sum)
     return (
         <div className='home__main'>
             <div className="row g-0">
@@ -30,17 +32,30 @@ const Home = () => {
                             <h1 className='home__content__heading'>Ultra Fitness Club</h1>
                             <p className='home__content__heading__sub'>Select today's exercise</p>
                             <div className="row g-3">
-                            {
-                                exercise.map( data => <ExerciseData key={data.id} allData={data} add={add}></ExerciseData> )
-                            }
+                                {
+                                    exercise.map(data => <ExerciseData key={data.id} allData={data} add={add}></ExerciseData>)
+                                }
 
+                            </div>
                         </div>
-                        </div>
-                        
+
                     </div>
                 </div>
                 <div className="col-md-3 col-sm-12 p-5">
-                    <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Soluta natus temporibus adipisci itaque recusandae? Corporis laudantium a temporibus perspiciatis voluptates architecto omnis sit itaque animi corrupti fugit incidunt doloribus distinctio, facilis quidem accusantium earum quisquam dolorum aut? Aut, laboriosam tempora.</p>
+                    <div className="profile">
+                        <div className="profile__content d-flex">
+                            <img className='pro__pic' src={proPic} alt="" />
+                            <div className="profile__content__detail">
+                                <p className='profile__name'>John Doe</p>
+                                <div className="profile__detail d-flex">
+                                    <FiMapPin></FiMapPin>
+                                    <span className='address'>Sydney, Australia</span>
+                                </div>
+                            </div>
+
+                        </div>
+
+                    </div>
                 </div>
             </div>
         </div>
