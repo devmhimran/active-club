a9b3c2bc50fcb24a0ca6107325fb5319cc52c0f8
Mahmud Hasan Imran <mahmud.bubt.150@gmail.com>
2022-09-30 16:32:27 +0600

    Update Added Button

--- src/Component/ExerciseData/ExerciseData.js
+++ src/Component/ExerciseData/ExerciseData.js
@@ -1,7 +1,12 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 const ExerciseData = ({ allData, add }) => {
     const { id, img, name, description, age, time } = allData;
+    const [text, setText] = useState('');
+    const handleAddedButton = (data) =>{
+        setText('Added');
+        add(data)
+    }
     return (
         <div className="col-lg-4 col-md-6 col-sm-12">
             <div className="card p-1" >
@@ -12,7 +17,7 @@ const ExerciseData = ({ allData, add }) => {
                     <p className="card-text">{description.slice(0, 75)}[...]</p>
                     <p className='age'>For Age : {age}</p>
                     <p className='time__require'>Time required : {time}s</p>
-                    <button onClick={()=> add(allData)} className='card__button'>Add to list</button>
+                    <button onClick={()=> handleAddedButton(allData)} className={`${text ? 'card__button__active' : '' } card__button `}>{text ? text : "Add to list"}</button>
                 </div>
             </div>
         </div>
